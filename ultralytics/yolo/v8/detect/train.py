7c3d125112f9906e9273ae503e400f09f573442b
CptCaptain <nilskoch@posteo.de>
2023-07-03 15:33:38 +0200

    allow augmentations to be passed as a dict via overrides
    update the overrides property of the model before the train() call.
    Add augmentation and probability as key and value.

--- ultralytics/yolo/v8/detect/train.py
+++ ultralytics/yolo/v8/detect/train.py
@@ -54,11 +54,12 @@ class DetectionTrainer(BaseTrainer):
         if 'CopyPaste' in self.data.get('type', ''):
             with torch_distributed_zero_first(rank):  # init dataset *.cache only once if DDP
                 base_dataset = self.build_dataset(dataset_path, mode, batch_size)
+                augmentations = self.data['augmentations'].update(self.args.copypaste_augmentations)
                 dataset = CopyPasteDataset(
                     base_dataset=base_dataset,
                     suppl_dataset_path=self.data['supplementary_dataset'],
                     split='train',
-                    augmentations=self.data['augmentations'],
+                    augmentations=augmentations,
                 )
         else:
             with torch_distributed_zero_first(rank):  # init dataset *.cache only once if DDP
